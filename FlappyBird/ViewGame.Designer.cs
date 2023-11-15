e76869b3c283d4a25bdb5eea795aea1488d0e7d7
RightCrite <fabian.brokatzky@gmail.com>
2023-11-15 10:57:56 +0100

    Moderlierung und basic UI wurden Erstellt und in der Projektmappe Hinzugefügt.

--- FlappyBird/ViewGame.Designer.cs
+++ FlappyBird/ViewGame.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pannelBoxRoehre1 = new System.Windows.Forms.PictureBox();
+            this.pannelBoxRoehre2 = new System.Windows.Forms.PictureBox();
+            this.pannelBoxRoehre3 = new System.Windows.Forms.PictureBox();
+            this.pannelBoxRoehre4 = new System.Windows.Forms.PictureBox();
+            this.pannelBoxVogel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxVogel)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pannelBoxRoehre1
+            // 
+            this.pannelBoxRoehre1.BackColor = System.Drawing.Color.Transparent;
+            this.pannelBoxRoehre1.Image = global::FlappyBird.Properties.Resources.Roehre;
+            this.pannelBoxRoehre1.Location = new System.Drawing.Point(687, 585);
+            this.pannelBoxRoehre1.Name = "pannelBoxRoehre1";
+            this.pannelBoxRoehre1.Size = new System.Drawing.Size(119, 353);
+            this.pannelBoxRoehre1.TabIndex = 0;
+            this.pannelBoxRoehre1.TabStop = false;
+            this.pannelBoxRoehre1.Click += new System.EventHandler(this.pannelBoxRoehre1_Click);
+            // 
+            // pannelBoxRoehre2
+            // 
+            this.pannelBoxRoehre2.BackColor = System.Drawing.Color.Transparent;
+            this.pannelBoxRoehre2.Image = global::FlappyBird.Properties.Resources.Roehre;
+            this.pannelBoxRoehre2.Location = new System.Drawing.Point(26, 585);
+            this.pannelBoxRoehre2.Name = "pannelBoxRoehre2";
+            this.pannelBoxRoehre2.Size = new System.Drawing.Size(120, 353);
+            this.pannelBoxRoehre2.TabIndex = 1;
+            this.pannelBoxRoehre2.TabStop = false;
+            // 
+            // pannelBoxRoehre3
+            // 
+            this.pannelBoxRoehre3.BackColor = System.Drawing.Color.Transparent;
+            this.pannelBoxRoehre3.Image = global::FlappyBird.Properties.Resources.Roehre_gedreht;
+            this.pannelBoxRoehre3.Location = new System.Drawing.Point(686, -256);
+            this.pannelBoxRoehre3.Name = "pannelBoxRoehre3";
+            this.pannelBoxRoehre3.Size = new System.Drawing.Size(120, 598);
+            this.pannelBoxRoehre3.TabIndex = 3;
+            this.pannelBoxRoehre3.TabStop = false;
+            this.pannelBoxRoehre3.Click += new System.EventHandler(this.pannelBoxRoehre3_Click);
+            // 
+            // pannelBoxRoehre4
+            // 
+            this.pannelBoxRoehre4.BackColor = System.Drawing.Color.Transparent;
+            this.pannelBoxRoehre4.Image = global::FlappyBird.Properties.Resources.Roehre_gedreht;
+            this.pannelBoxRoehre4.Location = new System.Drawing.Point(26, -256);
+            this.pannelBoxRoehre4.Name = "pannelBoxRoehre4";
+            this.pannelBoxRoehre4.Size = new System.Drawing.Size(120, 598);
+            this.pannelBoxRoehre4.TabIndex = 4;
+            this.pannelBoxRoehre4.TabStop = false;
+            // 
+            // pannelBoxVogel
+            // 
+            this.pannelBoxVogel.BackColor = System.Drawing.Color.Transparent;
+            this.pannelBoxVogel.Image = global::FlappyBird.Properties.Resources.vogel_icon;
+            this.pannelBoxVogel.Location = new System.Drawing.Point(317, 374);
+            this.pannelBoxVogel.Name = "pannelBoxVogel";
+            this.pannelBoxVogel.Size = new System.Drawing.Size(100, 75);
+            this.pannelBoxVogel.TabIndex = 5;
+            this.pannelBoxVogel.TabStop = false;
             // 
             // ViewGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 900);
+            this.BackgroundImage = global::FlappyBird.Properties.Resources.Hintergrund;
+            this.ClientSize = new System.Drawing.Size(850, 1024);
+            this.Controls.Add(this.pannelBoxVogel);
+            this.Controls.Add(this.pannelBoxRoehre4);
+            this.Controls.Add(this.pannelBoxRoehre3);
+            this.Controls.Add(this.pannelBoxRoehre2);
+            this.Controls.Add(this.pannelBoxRoehre1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ViewGame";
             this.Text = "ViewGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxRoehre4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pannelBoxVogel)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox pannelBoxRoehre1;
+        private PictureBox pannelBoxRoehre2;
+        private PictureBox pannelBoxRoehre3;
+        private PictureBox pannelBoxRoehre4;
+        private PictureBox pannelBoxVogel;
     }
 }
--- FlappyBird/ViewGame.cs
+++ FlappyBird/ViewGame.cs
@@ -16,5 +16,15 @@ namespace FlappyBird
         {
             InitializeComponent();
         }
+
+        private void pannelBoxRoehre1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pannelBoxRoehre3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

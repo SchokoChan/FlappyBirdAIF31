17e66fe554a831378944fe3b36c5f11c1c47176f
Jan <youtube.schololade@outlook.de>
2023-12-22 19:20:46 +0100

    False

--- FlappyBird/Program.cs
+++ FlappyBird/Program.cs
@@ -10,7 +10,7 @@ namespace FlappyBird
         [STAThread]
         static void Main()
         {
-            bool unitTest = true;
+            bool unitTest = false;
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
